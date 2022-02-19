08ab1eb200025c17293ac389d75fbbd3ac4c341f
nondanee <iminezn5656@outlook.com>
2022-02-19 22:38:36 +0800

    feat: init & turn

--- public/main.js
+++ public/main.js
@@ -1,6 +1,14 @@
 import Alpine from 'https://unpkg.com/alpinejs/dist/module.esm.js'
 
+import {
+    getPlayListDetail,
+    getSongDetail,
+    getSongUrl,
+} from './service.js'
+
 const player = () => ({
+    audio: new Audio(),
+
     cover: '',
     name: '',
     extra: '',
@@ -13,6 +21,14 @@ const player = () => ({
     mode: 0,
     random: false,
 
+    queue: [],
+    index: -1,
+
+    get id() {
+        if (!this.queue.length) return
+        return this.queue[this.index]
+    },
+
     get currentText() {
         return '--:--'
     },
@@ -22,16 +38,22 @@ const player = () => ({
     },
 
     onPlay() {
+        if (!this.queue.length) return
         this.paused = false
     },
     onPause() {
+        if (!this.queue.length) return
         this.paused = true
     },
     onNext() {
-
+        const { length } = this.queue
+        if (!length) return
+        this.index = (this.index + 1) % length
     },
     onPrevious() {
-
+        const { length } = this.queue
+        if (!length) return
+        this.index = (this.index - 1) % length
     },
     onRandomChange() {
         this.random = !this.random
@@ -39,6 +61,30 @@ const player = () => ({
     onModeChange() {
         this.mode = (this.mode + 1) % 3
     },
+
+    async init() {
+        this.$watch('id', async (id) => {
+            if (!id) return
+            const [song, url] = await Promise.all([
+                getSongDetail(id),
+                getSongUrl(id),
+            ])
+                .catch(() => {})
+
+            if (!url) {
+                this.onNext()
+                return
+            }
+
+            this.song = song
+            this.audio.src = url
+            // this.audio.play()
+        })
+
+        const data = await getPlayListDetail(2829896389)
+        this.queue = data.trackIds.map(({ id }) => id)
+        this.index = 0
+    }
 })
 
 Alpine.data('player', player)
